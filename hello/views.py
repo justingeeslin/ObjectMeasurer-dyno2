f01089ded08172c1480aa103f624ec8985901a81
Justin Geeslin <justin@geesl.in>
2026-03-05 14:12:02 -0600

    Adding measuring of one of the images in the repo

--- hello/views.py
+++ hello/views.py
@@ -3,13 +3,38 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Greeting
+import cv2
+from ObjectMeasurer import ObjectMeasurer
+import os
 
-# Create your views here.
-
+# SHORT SIDE / X-AXIS FIRST
+# A4_MM = (210.0, 297.0)
+LETTER_MM = (215.9, 279.4)  # 8.5in x 11in
 
 def index(request):
-    r = requests.get('https://httpbin.org/status/418', timeout=10)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    image_url = "https://raw.githubusercontent.com/justingeeslin/Real-Time-Object-Measurement/main/test-images/ucard-one-off-axis/ucard-one-off-axis.jpg"
+    image_name = "ucard-one-off-axis.jpg"
+
+    # import requests
+    import numpy as np
+    # import cv2
+
+    # url = "https://example.com/image.jpg"
+
+    r = requests.get(image_url)
+
+    # Convert bytes to numpy array
+    image_array = np.frombuffer(r.content, np.uint8)
+
+    # Decode image with OpenCV
+    img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+    # Construct the ObjectMeasurer with the size of the reference object
+    measurer = ObjectMeasurer(reference_size_mm=LETTER_MM)
+    # Get the measurements (in cm)
+    measurements = measurer.measure(img)
+
+    return HttpResponse(f'<h1>{[(m.width_cm, m.height_cm) for m in measurements]}</h1>')
 
 def db(request):
     # If you encounter errors visiting the `/db/` page on the example app, check that:
